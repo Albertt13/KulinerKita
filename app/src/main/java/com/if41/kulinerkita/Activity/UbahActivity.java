b630eeb3ce824d07e58e6c80ebdd8a3dec302cb4
Albertt13 <dhikaalbertus@mhs.mdp.ac.id>
2023-04-16 17:11:14 +0700

    16-04-2023 (design xml)

---
+++ app/src/main/java/com/if41/kulinerkita/Activity/UbahActivity.java
@@ -0,0 +1,18 @@
+package com.if41.kulinerkita.Activity;
+
+import static com.if41.kulinerkita.R.layout.activity_ubah;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.if41.kulinerkita.R;
+
+public class UbahActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(activity_ubah);
+    }
+}
