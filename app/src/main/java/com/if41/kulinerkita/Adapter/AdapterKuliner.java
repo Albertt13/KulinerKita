e4d9ad538d2b5da4569e3f936d28ea5684e4e870
Albertt13 <dhikaalbertus@mhs.mdp.ac.id>
2023-05-22 09:32:05 +0700

    22-05-2023 Update AdapterKuliner.java

--- app/src/main/java/com/if41/kulinerkita/Adapter/AdapterKuliner.java
+++ app/src/main/java/com/if41/kulinerkita/Adapter/AdapterKuliner.java
@@ -1,18 +1,31 @@
 package com.if41.kulinerkita.Adapter;
 
+import android.app.AlertDialog;
 import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.if41.kulinerkita.API.APIRequestData;
+import com.if41.kulinerkita.API.RetroServer;
+import com.if41.kulinerkita.Activity.MainActivity;
+import com.if41.kulinerkita.Activity.UbahActivity;
 import com.if41.kulinerkita.Model.ModelKuliner;
+import com.if41.kulinerkita.Model.ModelResponse;
 import com.if41.kulinerkita.R;
 
 import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class AdapterKuliner extends RecyclerView.Adapter<AdapterKuliner.VHKuliner> {
 
@@ -58,6 +71,60 @@ public class AdapterKuliner extends RecyclerView.Adapter<AdapterKuliner.VHKuline
             tvNama = itemView.findViewById(R.id.tv_nama);
             tvAsal = itemView.findViewById(R.id.tv_asal);
             tvDeskripsiSingkat = itemView.findViewById(R.id.tv_desc);
+
+            itemView.setOnLongClickListener(new View.OnLongClickListener() {
+                @Override
+                public boolean onLongClick(View v) {
+                    AlertDialog.Builder pesan = new AlertDialog.Builder(ctx);
+                    pesan.setTitle("Perhatian");
+                    pesan.setMessage("Operasi apa yang akan dilakukan?");
+                    pesan.setCancelable(true);
+
+                    pesan.setNegativeButton("Hapus", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            HapusKuliner(tvID.getText().toString());
+                            dialog.dismiss();
+                        }
+                    });
+
+                    pesan.setPositiveButton("Ubah", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            Intent varIntent = new Intent(ctx, UbahActivity.class);
+                            varIntent.putExtra("varID", tvID.getText().toString());
+                            varIntent.putExtra("varNama", tvNama.getText().toString());
+                            varIntent.putExtra("varAsal", tvAsal.getText().toString());
+                            varIntent.putExtra("varDesc", tvDeskripsiSingkat.getText().toString());
+                            ctx.startActivity(varIntent);
+                        }
+                    });
+
+                    pesan.show();
+                    return false;
+                }
+            });
+        }
+        //Method Hapus
+        private void HapusKuliner(String idKuliner) {
+            APIRequestData ARD = RetroServer.konekRetrofit().create(APIRequestData.class);
+            Call<ModelResponse> proses = ARD.ardDelete(idKuliner);
+
+            proses.enqueue(new Callback<ModelResponse>() {
+                @Override
+                public void onResponse(Call<ModelResponse> call, Response<ModelResponse> response) {
+                    String kode = response.body().getKode();
+                    String pesan = response.body().getPesan();
+
+                    Toast.makeText(ctx, "Kode: " + kode + ", Pesan: " + pesan, Toast.LENGTH_SHORT).show();
+                    ((MainActivity) ctx).retrieveKuliner();
+                }
+
+                @Override
+                public void onFailure(Call<ModelResponse> call, Throwable t) {
+                    Toast.makeText(ctx, "Gagal Menghubungi Server", Toast.LENGTH_SHORT).show();
+                }
+            });
         }
     }
 }
